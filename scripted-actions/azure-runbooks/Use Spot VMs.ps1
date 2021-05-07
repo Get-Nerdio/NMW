3504280d65e9eb28b8105462728561a08313e771
Nick Wagner <nwagner@getnerdio.com>
2021-05-07 16:37:04 -0500

    bug fixes for spot vm capacity error

--- scripted-actions/azure-runbooks/Use Spot VMs.ps1
+++ scripted-actions/azure-runbooks/Use Spot VMs.ps1
@@ -8,6 +8,9 @@ in which it is acceptable for a VM to be deallocated without warning
 
 See https://nmw.zendesk.com/hc/en-us/articles/360059485274-Scripted-Actions-Overview for more information on attaching scripted actions to VM deployments
 See https://docs.microsoft.com/en-us/azure/virtual-machines/spot-vms for more information on Spot VMs and pricing
+
+NOTE: if there is insufficient capacity for the requested Spot VM size, this script will provision the VM with standard pricing.
+
 #>
 
 # Adjust Variables below to alter to your preference:
@@ -44,9 +47,18 @@ try {
     $NewVM = New-AzVM -ResourceGroupName $AzureResourceGroupName -Location $vm.Location -VM $NewVMConfig -Tag $vm.Tags
 }
 catch {
-    Write-Error "Error provisioning Spot VM. Deleting existing os disk and nic"
-    Write-Error $_.exception.message 
-    Write-Error $_.exception.innerexceptionmessage 
-    Remove-AzNetworkInterface -Name $vm.NetworkProfile.NetworkInterfaces[0].id -ResourceGroupName $AzureResourceGroupName -Force
-    Remove-AzDisk -ResourceGroupName $AzureResourceGroupName -DiskName $vm.StorageProfile.OsDisk.Name -Force
+    Write-Output $_.message 
+    Write-Output $_.innerexception.message 
+    Write-Output "ERROR: Error provisioning Spot VM, possibly due to capacity limitations. Attempting to re-provision as standard vm"
+    try {
+        $NewVMConfig = New-AzVMConfig  -VMName $AzureVmName -VMSize $vm.HardwareProfile.VmSize -LicenseType $vm.LicenseType
+        $NewVMConfig = Add-AzVMNetworkInterface -VM $NewVMConfig -Id $vm.NetworkProfile.NetworkInterfaces[0].id
+        $NewVMConfig = Set-AzVMOSDisk -VM $NewVMConfig -ManagedDiskId $vm.StorageProfile.OsDisk.ManagedDisk.id -Name $vm.StorageProfile.OsDisk.Name -CreateOption Attach -StorageAccountType $vm.StorageProfile.OsDisk.ManagedDisk.StorageAccountType -Windows
+        $NewVM = New-AzVM -ResourceGroupName $AzureResourceGroupName -Location $vm.Location -VM $NewVMConfig -Tag $vm.Tags
+    }
+    catch {
+        write-output "ERROR: Unable to provision VM. Removing NIC and OS disk."
+        Remove-AzNetworkInterface -Name $vm.NetworkProfile.NetworkInterfaces[0].id -ResourceGroupName $AzureResourceGroupName -Force
+        Remove-AzDisk -ResourceGroupName $AzureResourceGroupName -DiskName $vm.StorageProfile.OsDisk.Name -Force
+    }
 }
