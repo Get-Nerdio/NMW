c55013cbc20e8c39256d1e5841f1596823a129a4
bitdeft <brycetorian@gmail.com>
2021-05-07 17:05:06 -0700

    Update Install Zoom VDI client.ps1
    updated logging to new format
    cleaned code format

--- scripted-actions/windows-scripts/Install Zoom VDI client.ps1
+++ scripted-actions/windows-scripts/Install Zoom VDI client.ps1
@@ -11,22 +11,22 @@ The plugin can be found here: https://support.zoom.us/hc/en-us/articles/36005298
 under "Windows Virtual Desktop"
 #>
 
-# Configure powershell logging
-mkdir "C:\Windows\Temp\NMWLogs\ScriptedActions\zoom_sa" -Force
-Start-Transcript -Path "C:\Windows\Temp\NMWLogs\ScriptedActions\zoom_sa\ps_log.log" -Append
-Write-Host "######################################
-New Script Run: Install Zoom VDI Client"
+# Start powershell logging
+$SaveVerbosePreference = $VerbosePreference
+$VerbosePreference = 'continue'
 $VMTime = Get-Date
-$Time = $VMTime.ToUniversalTime()
-Write-Host "Current time (UTC-0): $Time"
+$LogTime = $VMTime.ToUniversalTime()
+mkdir "C:\Windows\temp\NMWLogs\ScriptedActions\zoom_sa" -Force
+Start-Transcript -Path "C:\Windows\temp\NMWLogs\ScriptedActions\zoom_sa\ps_log.txt" -Append
+Write-Host "################# New Script Run #################"
+Write-host "Current time (UTC-0): $LogTime"
 
 # Make directory to hold install files
-mkdir C:\Windows\Temp\zoom_sa\install
+mkdir "C:\Windows\Temp\zoom_sa\install" -Force
 
 # parse through the Zoom VDI Help page to get the most up-to-date download link
 Write-Host "Retrieving Zoom installer files. . ."
 $ZoomDlSite = Invoke-WebRequest "https://support.zoom.us/hc/en-us/articles/360052984292" -UseBasicParsing
-
 ForEach ($Href in $ZoomDLSite.Links.Href)
 {
     if ($Href -match "ZoomInstallerVDI" ){
@@ -34,6 +34,7 @@ ForEach ($Href in $ZoomDLSite.Links.Href)
         break
     }
 }
+
 # Download Zoom installer from Zoom Website
 Invoke-WebRequest -Uri $DLink -OutFile "C:\Windows\Temp\zoom_sa\install\ZoomInstallerVDI.msi" -UseBasicParsing
 
@@ -41,4 +42,6 @@ Invoke-WebRequest -Uri $DLink -OutFile "C:\Windows\Temp\zoom_sa\install\ZoomInst
 Write-Host "Installing Zoom. . ."
 Start-Process C:\Windows\System32\msiexec.exe -ArgumentList "/i C:\Windows\Temp\zoom_sa\install\ZoomInstallerVDI.msi /l*v C:\Windows\Temp\NMWLogs\ScriptedActions\zoom_sa\zoom_install_log.txt /qn /norestart"
 
+# End Logging
 Stop-Transcript
+$VerbosePreference=$SaveVerbosePreference
