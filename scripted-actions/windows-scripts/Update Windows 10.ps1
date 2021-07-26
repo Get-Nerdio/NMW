5bad8cba047046fdfad1643bb7ce5cb332d837e6
gmalec <87677188+gmalec@users.noreply.github.com>
2021-07-26 13:58:20 -0500

    Correct NuGet and PSGet order & spelling
    NuGet should be installed first, followed by PSGet - was reversed.
    Corrected spelling on 'initiate' , corrected Windows capitalization
    Removing trailing white space.

--- scripted-actions/windows-scripts/Update Windows 10.ps1
+++ scripted-actions/windows-scripts/Update Windows 10.ps1
@@ -3,22 +3,22 @@
 #tags: Nerdio
 <#
 Notes:
-This script will install ALL windows updates using PSWindowsUpdate 
+This script will install ALL Windows updates using PSWindowsUpdate
 See: https://www.powershellgallery.com/packages/PSWindowsUpdate for details on
 how to customize and use the module for your needs.
 #>
 
 # Ensure PSWindowsUpdate is installed on the system.
 if(!(Get-installedmodule PSWindowsUpdate)){
-    
+
     # Ensure NuGet and PowershellGet are installed on system
-    Install-Module PowershellGet -Force
     Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+    Install-Module PowershellGet -Force
 
     # Install latest version of PSWindowsUpdate
     Install-Module PSWindowsUpdate -Force
 }
-Import-module PSWindowsUpdate -Force
+Import-Module PSWindowsUpdate -Force
 
-# Initiaite download and install of all pending windows updates
+# Initiate download and install of all pending Windows updates
 Install-WindowsUpdate -AcceptAll -ForceInstall -IgnoreReboot
