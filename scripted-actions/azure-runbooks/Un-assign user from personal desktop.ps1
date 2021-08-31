818dd36a4c205eea14921b9f77cce07259d5c9f7
Nick Wagner <nwagner@getnerdio.com>
2021-08-31 11:51:59 -0500

    Bug fixes per devendra

--- scripted-actions/azure-runbooks/Un-assign user from personal desktop.ps1
+++ scripted-actions/azure-runbooks/Un-assign user from personal desktop.ps1
@@ -4,6 +4,8 @@
 <#
 Notes:
 
+This runbook only works on ARM (Spring 2020) host pools, not pre-ARM (Fall 2019)
+
 This runbook relies on the Az.DesktopVirtualization powershell module. 
 This module must be installed into the Nerdio manager scripted actions automation account. 
 See here for information on installing modules:
@@ -36,7 +38,7 @@ write-output "getting vm"
 $VM = Get-AzVM -ResourceGroupName $VMResourceGroupName -Name $AzureVMName
 
 $VMStatus = Get-AzVM -ResourceGroupName $VMResourceGroupName -Name $AzureVMName -Status
-if ($VMStatus.code -notmatch 'running') {
+if (!($VMStatus.Statuses.code -match 'running')) {
     Write-Output "Starting VM $AzureVMName"
     $VM | Start-AzVM 
 }
@@ -50,9 +52,16 @@ $RegistrationKey = New-AzWvdRegistrationInfo -ResourceGroupName $HostPoolResourc
 $tag = $vm.tags.Keys -match 'VM_FQDN'
 $AzureVMNameFQDN = $vm.Tags[$tag][0]
 
-# Removing Host from App Group
-$AppGroupName = "$HostPoolName-AppGroup"
-Write-Output "Removing host $AzureVMNameFQDN from the app group $AppGroupName"
+# Get host pool and app group
+$hp = Get-AzWvdHostPool -Name $HostPoolName -ResourceGroupName $HostPoolResourceGroupName
+$AppGroupName = ($hp.ApplicationGroupReference -split '/')[-1]
+
+# Removing user from App Group
+
+$SessionHost = Get-AzWvdSessionHost -HostPoolName $HostPoolName -Name $AzureVMNameFQDN -ResourceGroupName $HostPoolResourceGroupName
+$DesktopUser = $SessionHost.AssignedUser
+
+Write-Output "Removing user $desktopUser from the app group $AppGroupName"
 
 Remove-AzRoleAssignment -SignInName $DesktopUser -RoleDefinitionName 'Desktop Virtualization User' -ResourceName "$AppGroupName" -ResourceGroupName $HostPoolResourceGroupName -ResourceType 'Microsoft.DesktopVirtualization/applicationGroups' 
 
@@ -74,7 +83,10 @@ $Script | Out-File ".\RemoveAssignment.ps1"
 write-output "Execute local script on remote VM"
 Invoke-AzVMRunCommand -ResourceGroupName $VMResourceGroupName -VMName "$AzureVMName" -CommandId 'RunPowerShellScript' -ScriptPath '.\RemoveAssignment.ps1' 
 
-if ($VMStatus.code -notmatch 'running') {
+if (!($VMStatus.Statuses.code -match 'running'))  {
     write-output "stopping VM"
     $VM | Stop-AzVM -Force
+}
+else {
+    $VM | Restart-AzVM 
 }
