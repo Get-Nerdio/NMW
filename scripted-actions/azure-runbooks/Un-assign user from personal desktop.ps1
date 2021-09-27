d6a5ae2e067e14a10131cc6928e47001e30fe759
Nick Wagner <nwagner@getnerdio.com>
2021-09-27 12:30:34 -0500

    bug fix for no user assigned

--- scripted-actions/azure-runbooks/Un-assign user from personal desktop.ps1
+++ scripted-actions/azure-runbooks/Un-assign user from personal desktop.ps1
@@ -49,55 +49,55 @@ if (!($VMStatus.Statuses.code -match 'running')) {
     $VM | Start-AzVM 
 }
 
+try {
+    # Generate New Registration Token
+    Write-Output "Generate New Registration Token"
+    $RegistrationKey = New-AzWvdRegistrationInfo -ResourceGroupName $HostPoolResourceGroupName -HostPoolName $HostPoolName -ExpirationTime $((get-date).ToUniversalTime().AddDays(1).ToString('yyyy-MM-ddTHH:mm:ss.fffffffZ'))
+    
+    # Making AzureVMName Fully Qualified
+    $tag = $vm.tags.Keys -match 'VM_FQDN'
+    $AzureVMNameFQDN = $vm.Tags[$tag][0]
 
-# Generate New Registration Token
-Write-Output "Generate New Registration Token"
-$RegistrationKey = New-AzWvdRegistrationInfo -ResourceGroupName $HostPoolResourceGroupName -HostPoolName $HostPoolName -ExpirationTime $((get-date).ToUniversalTime().AddDays(1).ToString('yyyy-MM-ddTHH:mm:ss.fffffffZ'))
- 
-# Making AzureVMName Fully Qualified
-$tag = $vm.tags.Keys -match 'VM_FQDN'
-$AzureVMNameFQDN = $vm.Tags[$tag][0]
+    # Get host pool and app group
+    $hp = Get-AzWvdHostPool -Name $HostPoolName -ResourceGroupName $HostPoolResourceGroupName
+    $AppGroupName = ($hp.ApplicationGroupReference -split '/')[-1]
 
-# Get host pool and app group
-$hp = Get-AzWvdHostPool -Name $HostPoolName -ResourceGroupName $HostPoolResourceGroupName
-$AppGroupName = ($hp.ApplicationGroupReference -split '/')[-1]
+    $SessionHost = Get-AzWvdSessionHost -HostPoolName $HostPoolName -Name $AzureVMNameFQDN -ResourceGroupName $HostPoolResourceGroupName
+    $DesktopUser = $SessionHost.AssignedUser
 
-# Removing user from App Group
+    if ([string]::IsNullOrEmpty($DesktopUser)) {
+        Throw "No user is assigned to session host"
+    }
 
-$SessionHost = Get-AzWvdSessionHost -HostPoolName $HostPoolName -Name $AzureVMNameFQDN -ResourceGroupName $HostPoolResourceGroupName
-$DesktopUser = $SessionHost.AssignedUser
+    # Removing Host from the HostPool"
+    write-output "Removing Host $AzureVMNameFQDN from the HostPool $HostPoolName"
 
-if ($DesktopUser -eq $null) {
-    Write-Error "No user is assigned to session host"
-    exit
-}
-
-Write-Output "Removing user $desktopUser from the app group $AppGroupName"
-
-Remove-AzRoleAssignment -SignInName $DesktopUser -RoleDefinitionName 'Desktop Virtualization User' -ResourceName "$AppGroupName" -ResourceGroupName $HostPoolResourceGroupName -ResourceType 'Microsoft.DesktopVirtualization/applicationGroups' 
-
-# Removing Host from the HostPool"
-write-output "Removing Host $AzureVMNameFQDN from the HostPool $HostPoolName"
-
-Remove-AzWvdSessionHost -ResourceGroupName $HostPoolResourceGroupName -HostPoolName $HostPoolName -Name $AzureVMNameFQDN -Force 
- 
-# Execute local script on remote VM
-$Script = @"
+    Remove-AzWvdSessionHost -ResourceGroupName $HostPoolResourceGroupName -HostPoolName $HostPoolName -Name $AzureVMNameFQDN -Force 
+    
+    # Execute local script on remote VM
+    $Script = @"
 Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\RDInfraAgent\ -Name IsRegistered -Value 0
 Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\RDInfraAgent\ -Name RegistrationToken -Value $($RegistrationKey.Token)
 Restart-Service RDAgent
 Start-service RDAgentBootLoader
 "@
 $Script | Out-File ".\RemoveAssignment.ps1"
- 
-# Execute local script on remote VM
-write-output "Execute local script on remote VM"
-Invoke-AzVMRunCommand -ResourceGroupName $VMResourceGroupName -VMName "$AzureVMName" -CommandId 'RunPowerShellScript' -ScriptPath '.\RemoveAssignment.ps1' 
 
-if (!($VMStatus.Statuses.code -match 'running'))  {
-    write-output "stopping VM"
-    $VM | Stop-AzVM -Force
+    # Execute local script on remote VM
+    write-output "Execute local script on remote VM"
+    Invoke-AzVMRunCommand -ResourceGroupName $VMResourceGroupName -VMName "$AzureVMName" -CommandId 'RunPowerShellScript' -ScriptPath '.\RemoveAssignment.ps1' 
 }
-else {
-    $VM | Restart-AzVM 
+
+Catch {
+    Throw $_
+}
+
+Finally {
+    if (!($VMStatus.Statuses.code -match 'running'))  {
+        write-output "stopping VM"
+        $VM | Stop-AzVM -Force
+    }
+    else {
+        $VM | Restart-AzVM 
+    }
 }
