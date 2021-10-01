e25c69b4b38c45a239bd4ec9c455edcbcbdabddf
Nick Wagner <nwagner@getnerdio.com>
2021-10-01 14:26:35 -0500

    bug fix for stopped VM

--- scripted-actions/azure-runbooks/Set Regional Settings.ps1
+++ scripted-actions/azure-runbooks/Set Regional Settings.ps1
@@ -42,8 +42,8 @@ $JsonParams = '{
 # Set Error action
 $errorActionPreference = "Stop"
 
-# Get region settings from script or Nerdio Secure Variable
 
+# Get region settings from script or Nerdio Secure Variable
 
 
 if (!$JsonParams){
@@ -64,153 +64,171 @@ $azureContext = Set-AzContext -SubscriptionId $AzureSubscriptionId -ErrorAction 
 # Get the VM and reads out the tag
 $azVM = Get-AzVM -Name $AzureVMName -ResourceGroupName $AzureResourceGroupName
 
-
-# Convert tag value to PSCustomObject
-$regionalSettings =  $JsonParams | ConvertFrom-Json
-
-# Create the hashtable for the parameters
-$parameters = @{
-    nation = $regionalSettings.nation
-    mui = $regionalSettings.mui
-    muifallback = $regionalSettings.muifallback
-    locale = $regionalSettings.locale
-    keyboardLayout = $regionalSettings.keyboardLayout
+$VMStatus = Get-AzVM -ResourceGroupName $AzureResourceGroupName -Name $AzureVMName -Status
+if (!($VMStatus.Statuses.code -match 'running')) {
+    Write-Output "Starting VM $AzureVMName"
+    $azVM | Start-AzVM 
 }
 
-Write-Output ('INFO: Parameters for RunCommand: ' + ($parameters | Out-String))
+Try {
+    # Convert tag value to PSCustomObject
+    $regionalSettings =  $JsonParams | ConvertFrom-Json
 
-# Define script block to run remote
-$scriptBlock ={
-    param(
-        [string] $nation,
-        [string] $mui,
-        [string] $muifallback,
-        [string] $locale,
-        [string] $keyboardLayout
-    )
-
-    [array]$keyboardLayouts = $keyboardLayout.Split(',')
-
-    # Path to the xml file
-    [string]$xmlFile = "$PSScriptRoot\RegionalSettings.xml"
-
-    # Add fix for "The keyboard layout changes unexpectedly at logon"
-    # https://dennisspan.com/solving-keyboard-layout-issues-in-an-ica-or-rdp-session/#IgnoreRemoteKeyboardLayout
-    [string]$KeyboardLayoutPath = "HKLM:SYSTEM\CurrentControlSet\Control\Keyboard Layout\"
-    $IgnoreRemoteKeyboardLayout = (Get-ItemProperty -Path $KeyboardLayoutPath -Name "IgnoreRemoteKeyboardLayout" -ErrorAction SilentlyContinue).IgnoreRemoteKeyboardLayout
-    if($IgnoreRemoteKeyboardLayout -ne 1)
-    {
-        $null = New-ItemProperty -Path $KeyboardLayoutPath  -Name "IgnoreRemoteKeyboardLayout" -Value "1" -PropertyType DWORD -Force
+    # Create the hashtable for the parameters
+    $parameters = @{
+        nation = $regionalSettings.nation
+        mui = $regionalSettings.mui
+        muifallback = $regionalSettings.muifallback
+        locale = $regionalSettings.locale
+        keyboardLayout = $regionalSettings.keyboardLayout
     }
 
-    # Boolean to define the first InputLanguageId as the default
-    [bool]$firstrunKeyboardLayout = $true
+    Write-Output ('INFO: Parameters for RunCommand: ' + ($parameters | Out-String))
 
-    # Create XML
-    $xmlWriter = New-Object System.XMl.XmlTextWriter($xmlFile,$Null)
+    # Define script block to run remote
+    $scriptBlock ={
+        param(
+            [string] $nation,
+            [string] $mui,
+            [string] $muifallback,
+            [string] $locale,
+            [string] $keyboardLayout
+        )
 
-    # Basic settings
-    $xmlWriter.Formatting = 'Indented'
-    $xmlWriter.Indentation = 1
-    $XmlWriter.IndentChar = "`t"
+        [array]$keyboardLayouts = $keyboardLayout.Split(',')
 
-    # Create content (https://docs.microsoft.com/en-us/troubleshoot/windows-client/deployment/automate-regional-language-settings)
-    $xmlWriter.WriteStartDocument()
+        # Path to the xml file
+        [string]$xmlFile = "$PSScriptRoot\RegionalSettings.xml"
 
-    $xmlWriter.WriteStartElement("gs:GlobalizationServices")
-    $xmlWriter.WriteAttributeString("xmlns:gs","urn:longhornGlobalizationUnattend")
-
-        # User list
-        $xmlWriter.WriteStartElement("gs:UserList")
-            $xmlWriter.WriteStartElement("gs:User")
-            $xmlWriter.WriteAttributeString("UserID","Current")
-            $xmlWriter.WriteAttributeString("CopySettingsToDefaultUserAcct","true")
-            $xmlWriter.WriteAttributeString("CopySettingsToSystemAcct","true")
-            $xmlWriter.WriteEndElement()
-        $xmlWriter.WriteEndElement()
-
-        # GeoID
-        $xmlWriter.WriteStartElement("gs:LocationPreferences")
-            $xmlWriter.WriteStartElement("gs:GeoID")
-            $xmlWriter.WriteAttributeString("Value","$nation")
-            $xmlWriter.WriteEndElement()
-        $xmlWriter.WriteEndElement()
-
-        # MUI Languages
-        $xmlWriter.WriteStartElement("gs:MUILanguagePreferences")
-            $xmlWriter.WriteStartElement("gs:MUILanguage")
-            $xmlWriter.WriteAttributeString("Value","$mui")
-            $xmlWriter.WriteEndElement()
-
-            if (![string]::IsNullOrEmpty($muifallback)){
-                $xmlWriter.WriteStartElement("gs:MUIFallback")
-                $xmlWriter.WriteAttributeString("Value","$muifallback")
-                $xmlWriter.WriteEndElement()
-            }
-        $xmlWriter.WriteEndElement()
-
-        # Input preferences
-        $xmlWriter.WriteStartElement("gs:InputPreferences")
-        foreach($kbLayout in $keyboardLayouts)
+        # Add fix for "The keyboard layout changes unexpectedly at logon"
+        # https://dennisspan.com/solving-keyboard-layout-issues-in-an-ica-or-rdp-session/#IgnoreRemoteKeyboardLayout
+        [string]$KeyboardLayoutPath = "HKLM:SYSTEM\CurrentControlSet\Control\Keyboard Layout\"
+        $IgnoreRemoteKeyboardLayout = (Get-ItemProperty -Path $KeyboardLayoutPath -Name "IgnoreRemoteKeyboardLayout" -ErrorAction SilentlyContinue).IgnoreRemoteKeyboardLayout
+        if($IgnoreRemoteKeyboardLayout -ne 1)
         {
-                $xmlWriter.WriteStartElement("gs:InputLanguageID")
-                $xmlWriter.WriteAttributeString("Action","add")
-                $xmlWriter.WriteAttributeString("ID","$($kbLayout.Trim())")
-            if($firstrunKeyboardLayout)
+            $null = New-ItemProperty -Path $KeyboardLayoutPath  -Name "IgnoreRemoteKeyboardLayout" -Value "1" -PropertyType DWORD -Force
+        }
+
+        # Boolean to define the first InputLanguageId as the default
+        [bool]$firstrunKeyboardLayout = $true
+
+        # Create XML
+        $xmlWriter = New-Object System.XMl.XmlTextWriter($xmlFile,$Null)
+
+        # Basic settings
+        $xmlWriter.Formatting = 'Indented'
+        $xmlWriter.Indentation = 1
+        $XmlWriter.IndentChar = "`t"
+
+        # Create content (https://docs.microsoft.com/en-us/troubleshoot/windows-client/deployment/automate-regional-language-settings)
+        $xmlWriter.WriteStartDocument()
+
+        $xmlWriter.WriteStartElement("gs:GlobalizationServices")
+        $xmlWriter.WriteAttributeString("xmlns:gs","urn:longhornGlobalizationUnattend")
+
+            # User list
+            $xmlWriter.WriteStartElement("gs:UserList")
+                $xmlWriter.WriteStartElement("gs:User")
+                $xmlWriter.WriteAttributeString("UserID","Current")
+                $xmlWriter.WriteAttributeString("CopySettingsToDefaultUserAcct","true")
+                $xmlWriter.WriteAttributeString("CopySettingsToSystemAcct","true")
+                $xmlWriter.WriteEndElement()
+            $xmlWriter.WriteEndElement()
+
+            # GeoID
+            $xmlWriter.WriteStartElement("gs:LocationPreferences")
+                $xmlWriter.WriteStartElement("gs:GeoID")
+                $xmlWriter.WriteAttributeString("Value","$nation")
+                $xmlWriter.WriteEndElement()
+            $xmlWriter.WriteEndElement()
+
+            # MUI Languages
+            $xmlWriter.WriteStartElement("gs:MUILanguagePreferences")
+                $xmlWriter.WriteStartElement("gs:MUILanguage")
+                $xmlWriter.WriteAttributeString("Value","$mui")
+                $xmlWriter.WriteEndElement()
+
+                if (![string]::IsNullOrEmpty($muifallback)){
+                    $xmlWriter.WriteStartElement("gs:MUIFallback")
+                    $xmlWriter.WriteAttributeString("Value","$muifallback")
+                    $xmlWriter.WriteEndElement()
+                }
+            $xmlWriter.WriteEndElement()
+
+            # Input preferences
+            $xmlWriter.WriteStartElement("gs:InputPreferences")
+            foreach($kbLayout in $keyboardLayouts)
             {
-                $firstrunKeyboardLayout = $false
-                $xmlWriter.WriteAttributeString("Default","true")
+                    $xmlWriter.WriteStartElement("gs:InputLanguageID")
+                    $xmlWriter.WriteAttributeString("Action","add")
+                    $xmlWriter.WriteAttributeString("ID","$($kbLayout.Trim())")
+                if($firstrunKeyboardLayout)
+                {
+                    $firstrunKeyboardLayout = $false
+                    $xmlWriter.WriteAttributeString("Default","true")
+                }
+                    $xmlWriter.WriteEndElement()
             }
-                $xmlWriter.WriteEndElement()
-        }
-        $xmlWriter.WriteEndElement()
-
-        # User locale
-        $xmlWriter.WriteStartElement("gs:UserLocale")
-            $xmlWriter.WriteStartElement("gs:Locale")
-            $xmlWriter.WriteAttributeString("SetAsCurrent","true")
-            $xmlWriter.WriteAttributeString("Name","$locale")
             $xmlWriter.WriteEndElement()
+
+            # User locale
+            $xmlWriter.WriteStartElement("gs:UserLocale")
+                $xmlWriter.WriteStartElement("gs:Locale")
+                $xmlWriter.WriteAttributeString("SetAsCurrent","true")
+                $xmlWriter.WriteAttributeString("Name","$locale")
+                $xmlWriter.WriteEndElement()
+            $xmlWriter.WriteEndElement()
+
         $xmlWriter.WriteEndElement()
 
-    $xmlWriter.WriteEndElement()
+        # Write and close document
+        $xmlWriter.WriteEndDocument()
+        $xmlWriter.Flush()
+        $xmlWriter.Close()
 
-    # Write and close document
-    $xmlWriter.WriteEndDocument()
-    $xmlWriter.Flush()
-    $xmlWriter.Close()
+        # Write-PSFMessage -Level Host -Message 'Configure Regional Settings'
+        $null = control.exe "intl.cpl,,/f:`"$xmlFile`""
+        $null = Start-Sleep -Seconds 5
+        $null = Remove-Item -Path "$xmlFile" -Force
 
-    # Write-PSFMessage -Level Host -Message 'Configure Regional Settings'
-    $null = control.exe "intl.cpl,,/f:`"$xmlFile`""
-    $null = Start-Sleep -Seconds 5
-    $null = Remove-Item -Path "$xmlFile" -Force
+        # Create array with all keyboard ids
+        $keyBoardIDs = [System.Collections.ArrayList]@()
+        ForEach ($keyboardLayout in $keyboardLayouts){
+            $keyboardLayoutId = $keyboardLayout.Split(":")[1]
+            $null = $keyBoardIDs.Add($keyboardLayoutId)
+        }
 
-    # Create array with all keyboard ids
-    $keyBoardIDs = [System.Collections.ArrayList]@()
-    ForEach ($keyboardLayout in $keyboardLayouts){
-        $keyboardLayoutId = $keyboardLayout.Split(":")[1]
-        $null = $keyBoardIDs.Add($keyboardLayoutId)
-    }
-
-    # Remove all keyboard layouts that are not in the configuration and add the defined keyboard layouts
-    For ($i=1; $i -le 20; $i++)
-    {
-        $keyboardId = (Get-ItemProperty -Path 'HKCU:\Keyboard Layout\Preload' -Name $i -ErrorAction SilentlyContinue).$i
-        if($keyBoardIDs -notcontains $keyboardId)
+        # Remove all keyboard layouts that are not in the configuration and add the defined keyboard layouts
+        For ($i=1; $i -le 20; $i++)
         {
-            #Write-PSFMessage -Level Host -Message "Remove Keyboard Layout $i"
-            $null = Remove-ItemProperty -Path 'HKCU:\Keyboard Layout\Preload' -Name $i -ErrorAction SilentlyContinue
+            $keyboardId = (Get-ItemProperty -Path 'HKCU:\Keyboard Layout\Preload' -Name $i -ErrorAction SilentlyContinue).$i
+            if($keyBoardIDs -notcontains $keyboardId)
+            {
+                #Write-PSFMessage -Level Host -Message "Remove Keyboard Layout $i"
+                $null = Remove-ItemProperty -Path 'HKCU:\Keyboard Layout\Preload' -Name $i -ErrorAction SilentlyContinue
+            }
         }
     }
+
+    # Save the scriptblock to a file
+    $null = Set-Content -Path .\RegionalSettings.ps1 -Value $scriptBlock
+
+    # Run command on vm
+    $result = Invoke-AzVMRunCommand -ResourceGroupName $AzureResourceGroupName -VMName $AzureVMName -CommandId 'RunPowerShellScript' -ScriptPath .\RegionalSettings.ps1 -Parameter $parameters
+
+    Write-Output ('INFO: Result of RunCommand: ' + ($result | Out-String))
+
+    # Remove temporary file
+    $null = Remove-Item -Path .\RegionalSettings.ps1
 }
 
-# Save the scriptblock to a file
-$null = Set-Content -Path .\RegionalSettings.ps1 -Value $scriptBlock
+catch{
+    Throw $_
+}
 
-# Run command on vm
-$result = Invoke-AzVMRunCommand -ResourceGroupName $AzureResourceGroupName -VMName $AzureVMName -CommandId 'RunPowerShellScript' -ScriptPath .\RegionalSettings.ps1 -Parameter $parameters
-
-Write-Output ('INFO: Result of RunCommand: ' + ($result | Out-String))
-
-# Remove temporary file
-$null = Remove-Item -Path .\RegionalSettings.ps1
+Finally{
+    if (!($VMStatus.Statuses.code -match 'running'))  {
+        write-output "stopping VM"
+        $azVM | Stop-AzVM -Force
+    }
+}
