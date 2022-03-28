e6306a2ae2543de0b5b3849ceac93bc53b97d348
Nick Wagner <nwagner@getnerdio.com>
2022-03-28 17:50:51 -0500

    Changes for running against hosts

--- scripted-actions/azure-runbooks/Power on VMs for X Hours.ps1
+++ scripted-actions/azure-runbooks/Power on VMs for X Hours.ps1
@@ -3,9 +3,10 @@
 
 <# Notes:
 
-This script will turn on all VMs in a host pool, and ensure that Nerdio does not turn the VMs off again until
-X hours have passed. The number of hours is defined by the $RestrictScaleInForHours variable below, which defaults
-to 24. To use a different duration for the scale in restriction, clone this script and modify the variable below.
+When run against hosts in a host pool, this script will turn on all VMs and ensure that Nerdio does not turn the VMs 
+off again until X hours have passed. The number of hours is defined by the $RestrictScaleInForHours variable below, 
+which defaults to 24. To use a different duration for the scale in restriction, clone this script and modify the 
+variable below.
 
 #>
 
@@ -20,16 +21,12 @@ $Prefix = ($KeyVaultName -split '-')[0].ToUpper()
 $HostPoolRG = (Get-AzResource -ResourceId $HostpoolID).ResourceGroupName
 
 # Parse the VM names from the host names
-$VmNames = (Get-AzWvdSessionHost -HostPoolName $HostPoolName -ResourceGroupName $HostPoolRG).name | ForEach-Object {($_ -replace "$HostPoolName/",'' -split '\.')[0]}
-
-$VMs = $VmNames | ForEach-Object {Get-AzVM -Name $_ -ResourceGroupName $HostPoolRG}
+$VM = Get-AzVM -Name $AzureVMName -ResourceGroupName $AzureResourceGroupName
 $RestrictUntil = (Get-Date).AddHours($RestrictScaleInForHours)
 $TimeZoneId = (Get-TimeZone).id
 
-foreach ($VM in $VMs) {
-    # Set the scale in restriction tag to prevent Nerdio from turning the VMs off
-    $tags["$Prefix`_SCALE_IN_RESTRICTION"] = $RestrictUntil.ToString("yyyy-MM-ddTHH") + ";$TimeZoneId"
-    Set-AzResource -ResourceGroupName $vm.ResourceGroupName -Name $vm.name -ResourceType "Microsoft.Compute/VirtualMachines" -Tag $tags -Force 
-}
+$Tags = $vm.Tags
 
-$VMs | Start-AzVM 
+# Set the scale in restriction tag to prevent Nerdio from turning the VMs off
+$tags["$Prefix`_SCALE_IN_RESTRICTION"] = $RestrictUntil.ToString("yyyy-MM-ddTHH") + ";$TimeZoneId"
+Set-AzResource -ResourceGroupName $vm.ResourceGroupName -Name $vm.name -ResourceType "Microsoft.Compute/VirtualMachines" -Tag $tags -Force 
