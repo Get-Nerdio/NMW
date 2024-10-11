f3f6c9455e893b7040048fdc795b52fe8debb2e3
Losev Vyatcheslav <slosev@getnerdio.com>
2024-10-11 13:35:02 +0300

    Updated note

--- scripted-actions/windows-scripts/Enable Winget for CIS images (UAM).ps1
+++ scripted-actions/windows-scripts/Enable Winget for CIS images (UAM).ps1
@@ -4,6 +4,10 @@
 <#
 Notes:
 This script enables app installers by updating a registry key.
+
+IMPORTANT NOTE: This script is designed to offer a simple method to enabled Winget functionality on a CIS based image or other secure image. 
+Please ensure that running this script and enabling Winget does not conflict with your company security policies. If in doubt, DO NOT USE this script. 
+Please speak with your security team to establish if this exception may be allowed in your environment.
 #>
 
 $WinstationsKey = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\AppInstaller'
