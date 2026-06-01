46dca3ac42f59b50b8a68b76109744e3a1472c82
Losev Viacheslav <slosev@getnerdio.com>
2026-06-01 15:04:47 +0300

    updated link

--- scripted-actions/windows-scripts/Unregister Sophos Endpoint agent in Sophos Central.ps1
+++ scripted-actions/windows-scripts/Unregister Sophos Endpoint agent in Sophos Central.ps1
@@ -3,7 +3,7 @@
 <#
 Notes:
 IMPORTANT: Refer to the Sophos Integration Article for instructions on how to use this script!
-https://nmw.zendesk.com/hc/en-us/articles/1500004124602
+https://nmehelp.getnerdio.com/hc/en-us/articles/26124310620685-Integrate-Sophos-via-Scripted-Actions
 
 This script uses the Sophos API to delete the associated VM from Sophos Central.
 Please refer to sophos documentation for more information:
