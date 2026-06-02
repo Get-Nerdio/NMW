7500245129e149b522211dfe510586eee34d79f5
Losev Viacheslav <slosev@getnerdio.com>
2026-06-02 10:04:43 +0300

    updated kb links

--- scripted-actions/azure-runbooks/Use Spot VMs.ps1
+++ scripted-actions/azure-runbooks/Use Spot VMs.ps1
@@ -6,7 +6,7 @@ This script recreates a VM as a spot instance. This Scripted Action can be attac
 When used with a host pool, all VMs created for that host pool will be Spot VMs. This provides cost savings for testing/demo/non-production environments
 in which it is acceptable for a VM to be deallocated without warning
 
-See https://nmw.zendesk.com/hc/en-us/articles/360059485274-Scripted-Actions-Overview for more information on attaching scripted actions to VM deployments
+See https://nmehelp.getnerdio.com/hc/en-us/articles/26124327585421-Scripted-actions-overview for more information on attaching scripted actions to VM deployments
 See https://docs.microsoft.com/en-us/azure/virtual-machines/spot-vms for more information on Spot VMs and pricing
 
 NOTE: if there is insufficient capacity for the requested Spot VM size, this script will provision the VM with standard pricing.
